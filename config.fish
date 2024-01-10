c2d5feafc61f23f0dc48f25edc814f9064fe1130
Petchx <83633423+petchxx@users.noreply.github.com>
2024-01-10 07:07:18 +0000

    init

---
+++ config.fish
@@ -0,0 +1,41 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+set blue_color \#277fff
+set cyan_color \#5cb7a6
+
+function fish_prompt
+    set_color $cyan_color
+    echo -n "┌──("
+
+    set_color --bold $blue_color
+    echo -n "$USER@"(prompt_hostname)
+
+    set_color normal
+    set_color $cyan_color
+    echo -n ")─["
+    
+    set_color normal
+    set_color --bold
+    echo -n (prompt_pwd)
+
+    set_color normal
+    set_color $cyan_color
+    echo -n "]\n"
+    
+    set_color $cyan_color
+    echo -n "└─"
+
+    set_color --bold $blue_color
+    echo -n "\$ "
+
+    set_color normal
+end
+
+function postexec_test --on-event fish_postexec
+    echo 
+end
+
